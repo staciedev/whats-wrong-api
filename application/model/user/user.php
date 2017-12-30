4f063c06190dd3c5137efaaba1ff56991dde0335
staciedev <a.borenko@gmail.com>
2017-12-31 00:11:45 +0200

    Generating JSON Web Token for user auth

--- application/model/user/user.php
+++ application/model/user/user.php
@@ -1,5 +1,7 @@
 <?php
 namespace Whatswrong\User;
+use Whatswrong\App;
+use Firebase\JWT\JWT;
 
 class User {	
 	
@@ -31,8 +33,36 @@ class User {
 	
 	public function generate_token(): string 
 	{		
-		return 'dummy_token_' . time();
+		$token_id    = base64_encode( random_bytes(32) );
+    $issued_at   = time();
+    $not_before  = $issued_at + 10; // Adding 10 seconds
+    $expire     = $not_before + 3600 * 3; // Adding 3 hours
+    $server_name = App::$complete_url; // Server name will be a domain name    
+    
+    // Create the token as an array     
+    $data = [
+        'iat'  => $issued_at, // Issued at: time when the token was generated
+        'jti'  => $token_id, // Json Token Id: an unique identifier for the token
+        'iss'  => $server_name, // Issuer
+        'nbf'  => $not_before, // Not before
+        'exp'  => $expire, // Expire
+        'data' => [
+            'userId'   => $this->id, // User ID from the users table
+            'userEmail' => $this->email, // User email
+        ]
+    ];
+		
+		$secret_key = App::$config::JWT_SECRET_KEY;
+		
+		$jwt = JWT::encode(
+        $data,
+        $secret_key,
+        'HS512' // Algorithm used to sign the token, see https://tools.ietf.org/html/draft-ietf-jose-json-web-algorithms-40#section-3
+        );        
+    
+		return $jwt;
 	}
+	
 	
 	public function reset_token() 
 	{
